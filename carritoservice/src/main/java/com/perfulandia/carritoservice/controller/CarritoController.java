5338bc94ecdd450470429093f224de7eb0916a59
alvidi12 <an.villarroeld@duocuc.cl>
2025-05-23 20:14:54 -0400

    cosa mala

--- carritoservice/src/main/java/com/perfulandia/carritoservice/controller/CarritoController.java
+++ carritoservice/src/main/java/com/perfulandia/carritoservice/controller/CarritoController.java
@@ -42,6 +42,11 @@ public class CarritoController {
     //Nuevo metodo trayendo a producto
     @GetMapping("/producto/{id}")
     public Producto obtenerProducto(@PathVariable long id) {
-        return restTemplate.getForObject("http://localhost:8082/carrito/producto/" + id, Producto.class);
+        return restTemplate.getForObject("http://localhost:8082/productos/" + id, Producto.class);
+    }
+
+    @GetMapping("/producto/listar")
+    public Producto[] productos() {
+        return restTemplate.getForObject("http://localhost:8082/productos", Producto[].class);
     }
 }
